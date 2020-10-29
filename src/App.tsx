33a79c0f56647deba0cd06861a3fa7769fcf0a81
Aureo Vitor <aureovitor@gmail.com>
2020-10-29 13:38:54 -0300

    add state management and derived component behavior

--- src/App.tsx
+++ src/App.tsx
@@ -8,43 +8,59 @@ import {
   QuizzContent,
   Score,
 } from "./components";
-
-type Question = {
-  category: string;
-  type: string;
-  dificult: "easy" | "medium" | "hard";
-  text: string;
-  correct: string;
-  incorrect: string[];
-};
+import { Question } from "./AppTypedefs";
+import { appReducer, initState, AppContext } from "./AppContext";
 
 const uri = `https://gist.githubusercontent.com/antoniopresto/
 ac901a4eaa57ca7c53e6344db882d499/raw/
 7b8ef5b706c2640f9b833fd02407204e89007113/questions.json`;
 
 export const App = () => {
+  const [appState, dispatch] = React.useReducer(appReducer, initState);
   const { response } = useCustomFetch<Question[]>(uri);
+
+  const {
+    currentIndex,
+    currntQuestion,
+    questionList,
+    correctGuesses,
+    wrongGuesses,
+  } = appState;
+
+  React.useEffect(() => {
+    if (response.type === "success")
+      dispatch({ type: "init", questions: response.data });
+  }, [response]);
 
   return (
     <StyleWrapper>
       <PageContainer>
-        <QuizzContainer>
-          <ProgressBar />
-          {response.type === "pending" && <h1>Loading...</h1>}
-          {response.type === "success" && (
-            <QuizzContent
-              category={response.data[0].category}
-              type={response.data[0].type}
-              dificult={response.data[0].dificult}
-              questiontext={response.data[0].text}
-              answers={response.data[0].incorrect.concat(
-                response.data[0].correct
-              )}
+        <AppContext.Provider value={[appState, dispatch]}>
+          <QuizzContainer>
+            <ProgressBar
+              current={currentIndex + 1}
+              total={questionList.length}
             />
-          )}
-          {response.type === "error" && <h1>{response.msg}</h1>}
-          <Score />
-        </QuizzContainer>
+            {response.type === "pending" && <h1>Loading...</h1>}
+            {response.type === "success" && (
+              <QuizzContent
+                category={currntQuestion.category}
+                type={currntQuestion.type}
+                dificult={currntQuestion.dificult}
+                questiontext={currntQuestion.text}
+                answers={currntQuestion.incorrect.concat(
+                  currntQuestion.correct
+                )}
+              />
+            )}
+            {response.type === "error" && <h1>{response.msg}</h1>}
+            <Score
+              totalCount={questionList.length}
+              correctCount={correctGuesses}
+              wrongCount={wrongGuesses}
+            />
+          </QuizzContainer>
+        </AppContext.Provider>
       </PageContainer>
     </StyleWrapper>
   );
